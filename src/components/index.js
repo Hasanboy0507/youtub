8444d511c200c917069e31a5b6ae7fe0c208c942
Your Name <you@example.com>
2025-03-25 17:02:03 +0500

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+export {default as App} from './app/app'
+export {default as Main} from './main/main'
+export {default as Search} from './search/search'
+export {default as VideoDetail} from './video-detial/videoDetail'
+export {default as Chanel} from './chanel/chanel'
+export {default as Navbar} from './navbar/navbar'
+export {default as SearchBox} from './search-box/searchBox'
+export {default as Category} from './category/category'
+
